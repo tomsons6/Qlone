5b18ae4335e52158732bb2497f59ebe9bc4e4699
tomsons6 <toms.amsons@va.lv>
2020-12-28 16:03:46 +0200

    Update

---
+++ QloneV2/Assets/Scripts/Character/InputManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class InputManager : MonoBehaviour
+{
+    [Header("Q")]
+    public KeyCode KeyboardButtonQ;
+    public UnityEvent OnQDown = new UnityEvent();
+    public UnityEvent OnQUp = new UnityEvent();
+    public UnityEvent OnQHold = new UnityEvent();
+
+    [Header("F")]
+    public KeyCode KeyboardButtonF;
+    public UnityEvent OnFDown = new UnityEvent();
+    public UnityEvent OnFUp = new UnityEvent();
+    public UnityEvent OnFHold = new UnityEvent();
+
+    [Header("E")]
+    public KeyCode KeyboardButtonE;
+    public UnityEvent OnEDown = new UnityEvent();
+    public UnityEvent OnEUp = new UnityEvent();
+    public UnityEvent OnEHold = new UnityEvent();
+
+    void Update()
+    {
+        //--------Q button------
+        if (Input.GetKeyDown(KeyboardButtonQ))
+        {
+            OnQDown.Invoke();
+        }
+        if (Input.GetKeyUp(KeyboardButtonQ))
+        {
+            OnQUp.Invoke();
+        }
+        if(Input.GetKey(KeyboardButtonQ))
+        {
+            OnQHold.Invoke();
+        }
+        //--------F button------
+        if (Input.GetKeyDown(KeyboardButtonF))
+        {
+            OnFDown.Invoke();
+        }
+        if (Input.GetKeyUp(KeyboardButtonF))
+        {
+            OnFUp.Invoke();
+        }
+        if (Input.GetKey(KeyboardButtonF))
+        {
+            OnFHold.Invoke();
+        }
+        //--------E button------
+        if (Input.GetKeyDown(KeyboardButtonE))
+        {
+            OnEDown.Invoke();
+        }
+        if (Input.GetKeyUp(KeyboardButtonE))
+        {
+            OnEUp.Invoke();
+        }
+        if (Input.GetKey(KeyboardButtonE))
+        {
+            OnEHold.Invoke();
+        }
+    }
+}
